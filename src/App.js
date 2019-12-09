09383b9d88dc170c06e03f6fd77ca22f3b42cdfb
ngoducanh179 <46935024+ngoducanh179@users.noreply.github.com>
2019-12-09 13:58:09 +0700

    hgeuwhgwe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-
+import axios from 'axios';
 function App() {
+  const [ppm, Setppm] = useState(0);
+  useEffect(async () => {
+    const res = await axios.get(
+      'http://188.166.206.43/64OQlQhCGUF2TkazMwsPiw3m5QI20Ph1/get/V6'
+    );
+
+    await Setppm(res.data[0]);
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <h4>
+          <h3>ppm :</h3>
+          {ppm && ppm}
+        </h4>
+      </div>
     </div>
   );
 }
